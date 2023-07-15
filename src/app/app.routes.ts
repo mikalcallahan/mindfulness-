628f00f446badb65c361e352d16540ad5807d88e
Mikal Callahan <mikal@dovera.local>
2023-07-14 22:50:53 -0400

    02-projects WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: AboutComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
   }
 ];
---
+++ src/app/projects/interfaces/index.ts
@@ -0,0 +1,5 @@
+export interface Project {
+  name: string;
+  link: string;
+  rolse?: string;
+}
