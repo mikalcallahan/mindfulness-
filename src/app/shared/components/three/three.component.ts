16144d8e764bd2fb36896d40544a712203f3017c
Mikal Callahan <mikal@dovera.local>
2023-07-16 20:09:56 -0400

    ThreeComponent enhancements

--- src/app/shared/components/three/three.component.ts
+++ src/app/shared/components/three/three.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BoxGeometry, Color, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import {
+  BoxGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // import * as THREE from 'three/src/Three';
@@ -9,52 +17,30 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
   selector: 'three',
   standalone: true,
   imports: [CommonModule],
-  template: ``,
-  // templateUrl: './three.component.html',
+  templateUrl: './three.component.html',
   styleUrls: ['./three.component.scss'],
 })
-export class ThreeComponent {
-  ngOnInit(): void {
+export class ThreeComponent implements AfterViewInit {
+  @ViewChild('threeContainer') threeContainer!: ElementRef;
+  ngAfterViewInit(): void {
+    console.log('threeContainer', this.threeContainer)
     const scene: Scene = new Scene();
-    scene.background = new Color(0x553224);
+    scene.background = new Color(objectColor);
+    scene.add(cube);
 
-    const camera: PerspectiveCamera = new PerspectiveCamera(
-      75,
-      window.innerWidth / 2 / (window.innerHeight / 2),
-      0.1,
-      1000,
-    );
+    const canvas: HTMLCanvasElement =
+      this.threeContainer.nativeElement.appendChild(renderer.domElement);
 
-    const renderer: WebGLRenderer = new WebGLRenderer();
-    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-    const canvas: HTMLCanvasElement = document.body.appendChild(
-      renderer.domElement,
-    );
     canvas.width = window.innerWidth / 2;
     canvas.height = window.innerHeight / 2;
 
-    new OrbitControls(camera, renderer.domElement);
-
-    const geometry: BoxGeometry = new BoxGeometry();
-    const material: MeshBasicMaterial = new MeshBasicMaterial({
-      color: 0x00ff00,
-      wireframe: true,
-    });
-
-    const cube: Mesh = new Mesh(geometry, material);
-
-    scene.add(cube);
     camera.position.z = 2;
-
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.5);
-    }
 
     window.addEventListener('resize', onWindowResize, false);
 
-    const animate = function () {
+    new OrbitControls(camera, renderer.domElement);
+
+    const animate = () => {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
@@ -64,3 +50,29 @@ export class ThreeComponent {
     animate();
   }
 }
+
+const backgroundColor = 0xe5e7e6;
+const objectColor = 0x141301;
+
+const geometry: BoxGeometry = new BoxGeometry();
+const material: MeshBasicMaterial = new MeshBasicMaterial({
+  color: backgroundColor,
+  wireframe: true,
+});
+const cube: Mesh = new Mesh(geometry, material);
+
+const camera: PerspectiveCamera = new PerspectiveCamera(
+  75,
+  window.innerWidth / 2 / (window.innerHeight / 2),
+  0.1,
+  1000
+);
+
+const renderer: WebGLRenderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.5);
+};
