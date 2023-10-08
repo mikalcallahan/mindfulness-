79aa1ec6f68c6f0b1fc89176403cf4087b16ebb5
Mikal Callahan <mikal@dovera.local>
2023-10-08 16:58:54 -0400

    16 - wip - see if this works in staging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent, ThreeComponent } from './shared/components';
 
+import '../styles.scss';
+
 @Component({
   selector: 'app-root',
   standalone: true,
