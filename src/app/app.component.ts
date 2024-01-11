ecb87604080eece392371fa05a2e1a4530ed0d95
Mikal Callahan <mikal@dovera.local>
2024-01-11 08:09:37 -0500

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent, ThreeComponent } from './shared/components';
 
-// import '../styles.scss';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [NavbarComponent, RouterOutlet, ThreeComponent],
   templateUrl: './app.component.html',
-  // template: ` <router-outlet></router-outlet> `,
-  /*
-  styles: [
-    `
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
-    `,
-  ],
-  */
 })
 export class AppComponent {}
