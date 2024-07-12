e5bfbb398f6952711f80f7daa33cc8b22f5939a3
Mikal Callahan <mikal.callahan@icloud.com>
2024-07-12 18:53:26 -0400

    chore: fix ng lint errors

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,9 +1,1 @@
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { NavbarComponent, ThreeComponent } from './components';
-
-// const DefaultModules = [CommonModule];
-// const Components = [NavbarComponent, ThreeComponent];
-
-// export const Shared = [...DefaultModules, ...Components];
 export * from './components';
