b29dc2f66a815227c10cf9080144bc792c24f79e
Mikal Callahan <mikal.callahan@icloud.com>
2024-07-29 16:44:58 -0400

    3 - feat: add moment privacy policy md

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,13 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog({
+      prerender: {
+        routes: ['', '/moment'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
