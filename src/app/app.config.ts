52758f43173e71b2afc0cd6a5ccfd4484b23eeff
Mikal Callahan <mikal@dovera.local>
2026-03-09 20:13:18 -0700

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
+import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { withPrismHighlighter } from '@analogjs/content/prism-highlighter';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideFileRouter(),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([requestContextInterceptor])
+    ),
+    provideClientHydration(withEventReplay()),
+    provideContent(withMarkdownRenderer(), withPrismHighlighter()),
+  ],
+};
---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterLink, RouterOutlet],
+  template: `
+    <nav>
+      <a routerLink="/">Home</a>
+    </nav>
+
+    <router-outlet />
+  `,
+  styles: `
+    :host {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+
+    nav {
+      text-align: left;
+      padding: 0 0 2rem 0;
+    }
+  `,
+})
+export class App {}
